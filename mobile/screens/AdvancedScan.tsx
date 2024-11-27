807e7ac35ca0168740f3a8457d692d3008139b3e
Sadrac Aramburo <sadrac.aramburo@gmail.com>
2024-11-26 19:12:18 -0600

    borkon sorkon commit

--- mobile/screens/AdvancedScan.tsx
+++ mobile/screens/AdvancedScan.tsx
@@ -4,23 +4,40 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useFonts } from 'expo-font';
+import { RootStackParamList } from '../App';
 
+type AdvancedScanProps = {
+  navigation: StackNavigationProp<RootStackParamList, 'AdvancedScans'>;
+};
 
-const AdvancedScan = () => (
-  <View className="flex-1 p-5">
-    <View className="flex-col justify-between items-center w-full mb-6">
-      
-    <Text style={{ fontFamily: 'Vercel-semi', fontSize: 40 }} className="mt-20 mb-5 text-center">Advanced Scan</Text>
+const AdvancedScans = ({ navigation }: AdvancedScanProps) => {
+  const advancedScanTools = [
+    { name: 'WebApp Scan', image: require('../assets/bancoCogs.jpg') , desc : "WebApp Scan is a tool that scans web applications for vulnerabilities and security issues."},
+    { name: 'Host Discovery', image: require('../assets/bancoCogs.jpg') ,desc : "Host Discovery is a tool that scans a network for active hosts and devices."},
+    { name: 'Malware Scan', image: require('../assets/bancoCogs.jpg') , desc : "Malware Scan is a tool that scans files and directories for malware and viruses."},
+   
+  ];
+ 
+  return (
+    <View className="flex p-5 items-center">
+      <View className="flex-col justify-between items-center w-full ">
+        <Text style={{ fontFamily: "Vercel-semi", fontSize: 40 }} className="text-center">Advanced Scanning</Text>
+      </View>
+      <Text style={{ fontFamily: "Vercel-semi", fontSize: 25 }} className="text-xl font-bold mb-1 text-gray-700" >Tools</Text>
+      <View className="flex-row flex-wrap justify-around">
+        {advancedScanTools.map((tool, index) => (
+          <TouchableOpacity
+            key={index}
+            className="w-[45%] bg-black rounded-lg p-5 m-1 items-center shadow-xl shadow-black"
+            onPress={() => navigation.navigate(tool.name.replace(' ', '') as keyof RootStackParamList)}>
+            <Image source={tool.image} className="w-[90%] h-28 mb-2" />
+            <Text className="text-center text-white" style={{ fontFamily: "Vercel-semi", fontSize: 15 }}>{tool.name}</Text>
+            <Text className="text-center text-gray-400 mt-2" style={{ fontFamily: "Vercel-semi", fontSize: 10 }}>{tool.desc}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
     </View>
-    <TextInput className="w-full h-10 border border-gray-400 rounded mb-3 px-3" placeholder="URL or IP to scan" />
-    <TouchableOpacity className="bg-black p-3 rounded-lg">
-      <Text className="text-white text-center">Start Scan</Text>
-    </TouchableOpacity>
-    <View>
-      <Text style={{ fontFamily: 'Vercel-semi', fontSize: 30 }} className="mt-6 mb-5 text-center text-gray-700">Scan Results and Vulnerabilities</Text>
-      <Text>results here</Text>
-    </View>
-  </View>
-);
+  );
+};
 
-export default AdvancedScan;
+export default AdvancedScans;
